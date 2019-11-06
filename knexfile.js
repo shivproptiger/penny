5b83ba7c1cbe3a033ff3748513232c67cb4e96d2
vishal <vishal.singal@fastfox.com>
2019-11-07 01:58:15 +0530

    rename env variables

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB,
+    database: process.env.DB_NAME,
   },
   pool: {
     min: 2,
